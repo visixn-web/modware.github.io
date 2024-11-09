4322f6e07fb51934e3ea1fe229c828ce18b52dfb
visixn-web <salloumfamily123@gmail.com>
2024-11-09 11:11:08 -0500

    Add files via upload

---
+++ website/js/frontend.js
@@ -0,0 +1,47 @@
+function loadd() {
+   document.getElementById('home').style.display = '';
+   document.getElementById('products').style.display = 'none';
+   document.getElementById('status').style.display = 'none';
+   document.getElementById('reviews').style.display = 'none';
+   document.getElementById('contact').style.display = 'none';
+}
+
+function home() {
+    document.getElementById('home').style.display = '';
+    document.getElementById('products').style.display = 'none';
+    document.getElementById('status').style.display = 'none';
+    document.getElementById('reviews').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+}
+function products() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('products').style.display = '';
+    document.getElementById('status').style.display = 'none';
+    document.getElementById('reviews').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+}
+function statuss() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('products').style.display = 'none';
+    document.getElementById('status').style.display = '';
+    document.getElementById('reviews').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+ }
+ function reviews() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('products').style.display = 'none';
+    document.getElementById('status').style.display = 'none';
+    document.getElementById('reviews').style.display = '';
+    document.getElementById('contact').style.display = 'none';
+ }
+ function contact() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('products').style.display = 'none';
+    document.getElementById('status').style.display = 'none';
+    document.getElementById('reviews').style.display = 'none';
+    document.getElementById('contact').style.display = '';
+ }
+ function login() {
+   alert("User login feature as well as a user dashboard will be added shortly");
+ }
+
